3392a91e3b90347df98e9f7b20b96733f6071453
Harish <harisiva.fb@gmail.com>
2023-11-23 13:06:59 +0530

    Q1108-shuffle the string

--- src/leetcode/Q1108.java
+++ src/leetcode/Q1108.java
@@ -3,13 +3,19 @@ package leetcode;
 import java.util.Arrays;
 
 public class Q1108 {
-    //defanging the String
-    public static void main(String[] args) {
-        System.out.println(defangIPaddr("1.1.1.1"));
+    //Shuffle the String
+    public static String restoreString(String s, int[] indices) {
+        char []ans = new char[indices.length];
+        for(int i=0; i<indices.length; i++) {
+            ans[indices[i]] = s.charAt(i);
+        }
+        return new String(ans);
     }
-    public static String defangIPaddr(String address) {
-        String name = address.replace(".","[.]");
 
-        return  (name);
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = {4,5,6,7,0,2,1,3};
+        System.out.println(restoreString(s,indices));
     }
+
 }
